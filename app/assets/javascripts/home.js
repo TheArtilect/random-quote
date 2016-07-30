d8aba875f33764ead47e53b2bcb8f88f60c7569d
Ian Agpawa <agpawaji@gmail.com>
2016-07-30 17:19:21 +0000

    added and modified

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -28,7 +28,7 @@ function startPage(){
       19: ["Time's funny. When you're a kid, it passes slowly, and next thing you're fifty and your childhood fits into a rusty little box.", "Bretodeau"],
       20: ["My name is Puggy and I live in a tree. I hope I didn't ruin anything for you.", "Puggy"],
       21: ["They carried all they could bear, and then some, including a silent awe for the power of the things they carried.", "Tim O'Brien"],
-      22: ["Bird law in this country is not governed by reason", "Charlie Kelly"],
+      22: ["Bird law in this country is not governed by reason.", "Charlie Kelly"],
       23: ["Hummingbirds are a legal tender.", "Charlie Kelly"],
       24: ["I'm having feelings again, like some kind of 14 year old kid or something.  You remember feelings, right?", "Dennis Reynolds"],
       25: ["Bro, when you tack on mass, you sacrifice flexibility. That's just a straight up fact.", "Mac"],
@@ -52,7 +52,14 @@ function startPage(){
       43: ["I don't know. I'm not gonna sit here and try and get inside the mind of a dog. I mean, that's God's work. Well, not that I believe in God.", "Rickety Cricket"],
       44: ["I love you, but that hasn't helped at all.", "Unknown"],
       45: ["There is a theory which states that if ever anybody discovers exactly what the Universe is for and why it is here, it will instantly disappear and be replaced by something even more bizarre and inexplicable. There is another theory which states that this has already happened.", "Douglas Adams"],
-      46: ["There was evidence in this room, of excessive consumption of almost every type of drug known to civilized man since 1544 A.D. It could only be explained as a montage.", "Hunter S. Thompson"]
+      46: ["There was evidence in this room, of excessive consumption of almost every type of drug known to civilized man since 1544 A.D. It could only be explained as a montage.", "Hunter S. Thompson"],
+      47: ["There he goes. One of God's own prototypes. A high-powered mutant of some kind never even considered for mass production. Too weird to live, and too rare to die.", "Raoul Duke"],
+      48: ["Strange memories on this nervous night in Las Vegas. Has it been five years? Six? It seems like a lifetime, the kind of peak that never comes again. San Francisco in the middle sixties was a very special time and place to be a part of. But no explanation, no mix of words or music or memories can touch that sense of knowing that you were there and alive in that corner of time in the world. Whatever it meant.", "Raoul Duke"],
+      49: ["With a bit of luck, his life was ruined forever. Always thinking that just behind some narrow door in all of his favorite bars, men in red woolen shirts are getting incredible kicks from things he'll never know.", "Raoul Duke"],
+      50: ["There was madness in any direction, at any hour. You could strike sparks anywhere. There was a fantastic universal sense that whatever we were doing was right, that we were winning.", "Raoul Duke"],
+      51: ["And that, I think, was the handle - that sense of inevitable victory over the forces of old and evil. Not in any mean or military sense; we didn't need that. Our energy would simply prevail. We had all the momentum; we were riding the crest of a high and beautiful wave. So now, less than five years later, you can go up on a steep hill in Las Vegas and look west, and with the right kind of eyes you can almost see the high-water mark - that place where the wave finally broke and rolled back.", "Raoul Duke"],
+      52: ["The fact that we live at the bottom of a deep gravity well, on the surface of a gas covered planet going around a nuclear fireball 90 million miles away and think this to be normal is obviously some indication of how skewed our perspective tends to be.", "Douglas Adams"],
+      53: ["We are going to die, and that makes us the lucky ones. Most people are never going to die because they are never going to be born. The potential people who could have been here in my place but who will in fact never see the light of day outnumber the sand grains of Arabia. Certainly those unborn ghosts include greater poets than Keats, scientists greater than Newton. We know this because the set of possible people allowed by our DNA so massively exceeds the set of actual people. In the teeth of these stupefying odds it is you and I, in our ordinariness, that are here.We privileged few, who won the lottery of birth against all odds, how dare we whine at our inevitable return to that prior state from which the vast majority have never stirred?", "Dr. Richard Dawkins"]
       
       // 22: ["lsdhflsdh flsd hflsdhf lsdhfls dfhlsd fhlsdhf lsdhfls dhfls hdflshdfls dhfls hdflshdfl shdfl shdlf hsdlfhsdlfhsldkfh sldkhf sldfhsdf", 'hsdofhlsdfhlsdkhflsdhflsdf']
     };
